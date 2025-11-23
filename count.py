e2bf52d8e19358058eddb535044ac0e3907afc03
Jiya Kalra <jiya25bai10733@gmail.com>
2025-11-23 22:13:35 +0530

    Intial commiy

--- count.py
+++
@@ -1,4 +0,0 @@
-from info import contacts
-
-def count():
-    print(f"Total contacts in your contact book: {len(contacts)}")
--- delete.py
+++
@@ -1,9 +0,0 @@
-from info import contacts
-
-def delete():
-    name = input("Enter name to delete from your contact book: ")
-    if name in contacts:
-        del contacts[name]
-        print(f"Contact with name {name} deleted!")
-    else:
-        print(f"{name} not foundin your contact book!")
